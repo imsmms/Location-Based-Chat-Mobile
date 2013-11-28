6bd0ce868880e4dc930936e4c4f5eabcaf070e05
ahmedk <ahmed.m.kamel@nileu.edu.eg>
2013-11-28 23:25:22 +0200

    updated chat code

--- LocationBasedChat/assets/www/js/chat.js
+++ LocationBasedChat/assets/www/js/chat.js
@@ -3,8 +3,9 @@ var socket;
 var receiver_id;
 
 function Inizialize(receiver){
-	var url = BASE_URL + CHAT_API;
-	socket = io.connect(BASE_URL + ':3000');
+	var url = BASE_URL;
+	socket = io.connect(BASE_URL);
+	//socket.emit('chat', {id: '5295d752192e68720f000005'});
 	socket.on('message', function(data) {
 		appendMessageToLog(data.message, data.sender);
 	});
@@ -13,10 +14,10 @@ function Inizialize(receiver){
 function sendMessage(message){
 	if(message == '')
 		return;
-	socket.emit('message', { Message: message, Receiver: receiver_id });
+	socket.emit('message', {to:'01008993983', txt:'Hello User'});
 	appendMessageToLog(message, 0);
 }
 
 function appendMessageToLog(message, sender) {
-	messageLog.add({ Message: message, Sender: sender });
+	//messageLog.add({ Message: message, Sender: sender });
 }
